e817ff3b7d3dd66b53b3d372f25a4df6d767da0d
Daniel <danielboone1990@gmail.com>
2021-11-25 22:35:12 +0700

    initialize server and client, setup react routes

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Chat = () => <h1>This is the chat component</h1>;
+
+export default Chat;
---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Join = () => <h1>This is the join component</h1>;
+
+export default Join;
