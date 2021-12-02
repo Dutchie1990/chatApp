a33e04220cf61fb9ad4f6c05bffc7de3752a55f1
Daniel <danielboone1990@gmail.com>
2021-12-02 21:40:18 +0700

    set up connection FE and BE

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import queryString from 'query-string';
+import { io } from 'socket.io-client';
+import { useLocation } from 'react-router';
 
-const Chat = () => <h1>This is the chat component</h1>;
+let socket;
+
+const Chat = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const location = useLocation();
+  const ENDPOINT = 'http://localhost:5000/';
+
+  useEffect(() => {
+    console.log();
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+    console.log(socket);
+
+    socket.emit('join', { name, room }, ({ error }) => {
+      alert(error);
+    });
+  }, [ENDPOINT, location.search]);
+
+  return <h1>Chat</h1>;
+};
 
 export default Chat;
--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,16 +7,32 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(router);
 
 io.on('connection', (socket) => {
   console.log('We have a connection');
+
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+
+    const error = true;
+
+    if (error) {
+      callback({ error: 'error' });
+    }
+  });
 
   socket.on('disconnect', () => {
     console.log('User had left');
   });
 });
-
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started at port ${PORT}`));
