f670a8e7375734bd0fe65ce713a7f25d7609c895
Daniel <danielboone1990@gmail.com>
2021-12-07 21:49:45 +0700

    adjust cors

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 const io = require('socket.io')(server, {
   cors: {
     origin:
-      'https://61af71bda18bcef28df4e189--laughing-fermat-2459b0.netlify.app/',
+      'https://61af71bda18bcef28df4e189--laughing-fermat-2459b0.netlify.app',
     methods: ['GET', 'POST'],
   },
 });
